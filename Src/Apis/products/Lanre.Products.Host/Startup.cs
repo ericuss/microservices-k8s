f91e036188f57d8a6de634d7f1f98a05053f1dc1
Eric Torre <etorreg@gmail.com>
2020-02-03 12:33:19 +0100

    Add addif extensions and configure https redirection

--- Src/Apis/products/Lanre.Products.Host/Startup.cs
+++ Src/Apis/products/Lanre.Products.Host/Startup.cs
@@ -38,12 +38,8 @@ namespace Lanre.Products.Host
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app
+                .AddIf(env.IsDevelopment(), x => x.UseDeveloperExceptionPage())
                 .UseApi(a => a.UseCustomSwagger(), endpoints => endpoints.UseHealthChecks())
                 .UseWelcomePage()
                 ;
